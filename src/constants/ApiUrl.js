4dbe7b950dae618636b8914d14df021c0287c2b8
Nam Nguyen <nguyennamcdt1@gmail.com>
2019-07-21 23:46:24 +0700

    updated

--- src/constants/ApiUrl.js
+++ src/constants/ApiUrl.js
@@ -1,6 +1,6 @@
-let apiEndPoint = 'http://api.successhub.us/api/v1/';
+let apiEndPoint = 'https://stagingapi.successhub.us/api/v1/';
 if (process.env.NODE_ENV === 'production') {
-  apiEndPoint = 'http://api.successhub.us/api/v1/';
+  apiEndPoint = 'https://stagingapi.successhub.us/api/v1/';
 }
 export const API_END_POINT = apiEndPoint;
 
